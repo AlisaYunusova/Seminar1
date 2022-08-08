865ea4b0ce225d587fb2448d201991099e25b09a
Alice <algus_91@mail.rugit config --global user.email algus_91@mail.ru>
2022-08-08 20:45:13 +0300

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine("Больше");
+    Console.WriteLine(number1);
+    Console.WriteLine("Меньше");
+    Console.WriteLine(number2);
+}
+else if (number1 < number2)
+{
+    Console.WriteLine("Больше");
+    Console.WriteLine(number2);
+    Console.WriteLine("Меньше");
+    Console.WriteLine(number1);
+}
+else if (number1 == number2)
+{
+    Console.WriteLine("Числа равны");
+}
