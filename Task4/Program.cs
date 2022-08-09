748eed06d576a93293e7073322438f62f047f3b5
Alice <algus_91@mail.rugit config --global user.email algus_91@mail.ru>
2022-08-09 12:00:13 +0300

    task4 done

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 1 число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int maxnumber = 0;
+
+if (number1 > number2)
+{
+    maxnumber = number1;
+}
+else 
+{
+    maxnumber = number2;
+}
+if (maxnumber > number3)
+{
+    Console.WriteLine(maxnumber);
+}
+else 
+{
+    Console.WriteLine(number3);
+}
