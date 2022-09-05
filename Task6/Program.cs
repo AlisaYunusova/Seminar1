a4043205081a4c6252de45535659757682db123c
AlisaYunusova <110364570+AlisaYunusova@users.noreply.github.com>
2022-09-05 17:31:31 +0300

    Task text

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите число");
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 
 if (number % 2 == 0)
